5b9040178d9136faf373cfd63eb9e2e68ea2f61a
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-09-10 21:59:53 -0600

    Creating URL's and methods

--- Router.php
+++ Router.php
@@ -3,7 +3,27 @@
 namespace MVC;
 
 class Router{
-    public function __construct(){
+
+    public $routesGET = [];
+    public $routesPOST = [];
+
+    public function get($url, $fn){
+        $this->routesGET[$url] = $fn;
+    }
+
+    public function checkRoutes(){
+        $currentURL = $_SERVER['PATH_INFO'] ?? '/';
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if($method === 'GET'){
+            $fn = $this->routesGET[$currentURL] ?? null;
+        }
+
+        if($fn){
+            call_user_func($fn, $this);
+        }else{
+            echo 'Page not found';
+        }
 
     }
 }
--- public/index.php
+++ public/index.php
@@ -3,3 +3,10 @@
 require_once __DIR__ .  '/../includes/app.php';
 
 use MVC\Router;
+
+$router = new Router();
+
+$router->get('/admin', 'admin_function');
+$router->get('/contact', 'contact_function');
+
+$router->checkRoutes();
