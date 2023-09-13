0934eea2e457c49028ae14c68e5f4830a96928cb
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-09-13 15:07:41 -0600

    Deployment: Test

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,5 +1,24 @@
 <?php
 function connect_db() : mysqli {
+    $host = 'mysql-leoc1302.alwaysdata.net';
+    $user = 'leoc1302';
+    $password = 'passWD123';
+    $database = 'leoc1302_realstate';
+
+    $db = new mysqli($host, $user, $password, $database);
+    if(!$db) {
+        echo "Error connecting to database";
+        exit;
+    }
+
+    return $db;
+}
+
+
+/*
+
+<?php
+function connect_db() : mysqli {
     $host = 'localhost';
     $user = 'root';
     $password = '123';
@@ -13,3 +32,5 @@ function connect_db() : mysqli {
 
     return $db;
 }
+
+*/
